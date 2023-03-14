a25ee4f488daf83fb74678d67b6ce465a3f0ac97
BrittsTest <brittanytest2301@gmail.com>
2023-03-14 20:52:51 +0000

    hashcode and equals

--- Animal.java
+++ Animal.java
@@ -12,5 +12,53 @@ public class Animal{
         System.out.println("I make an arbirary sound");
     }
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		result = prime * result + ((limbs == null) ? 0 : limbs.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((sound == null) ? 0 : sound.hashCode());
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Animal other = (Animal) obj;
+		if (age != other.age)
+			return false;
+		if (color == null) {
+			if (other.color != null)
+				return false;
+		} else if (!color.equals(other.color))
+			return false;
+		if (limbs == null) {
+			if (other.limbs != null)
+				return false;
+		} else if (!limbs.equals(other.limbs))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (sound == null) {
+			if (other.sound != null)
+				return false;
+		} else if (!sound.equals(other.sound))
+			return false;
+		return true;
+	}
+    
     
 }
